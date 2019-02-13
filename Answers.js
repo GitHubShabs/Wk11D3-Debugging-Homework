e080d119a4f1d179567ae27d83a4d8156d5386e3
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-02-13 20:09:35 +0000

    setup done

---
+++ Answers.js
@@ -0,0 +1,18 @@
+// Answer the following questions:
+// 1. What is the purpose of a breakpoint?
+// ITS PURPOSE IS TO
+
+
+// 2. Does the line of code on a breakpoint run when you start debugging?
+//
+
+
+// 3. How do we debug the next line of code?
+//
+
+
+// 4. What does the step into command do?
+//
+
+
+// 5. What is the difference between evaluate expression and evaluate code fragment?
