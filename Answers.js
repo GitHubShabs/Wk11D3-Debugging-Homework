57872a8d525f1b9cff60078ab5bcab42c29b68fe
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-02-13 21:01:37 +0000

    home work complete

--- Answers.js
+++ Answers.js
@@ -1,18 +1,17 @@
 // Answer the following questions:
 // 1. What is the purpose of a breakpoint?
-// ITS PURPOSE IS TO
-
+// IT PUTS A STOP/PAUSE THE APPLICATION AND LAUNCH A DEBUGGER CONSOLE.
 
 // 2. Does the line of code on a breakpoint run when you start debugging?
-//
-
+// NO, IT WONT RUN THAT LINE OF CODE.
 
 // 3. How do we debug the next line of code?
-//
-
+// HIGHLIGHT THAT LINE OF CODE (IE SELECT IT) THEN RIGHT CLICK ON THAT SELECTION AND CHOSE TO DEBUG THAT LINE.
 
 // 4. What does the step into command do?
-//
+// THIS COMMAND SWICHES TO THE METHOD ON THAT LINE AND PAUSES AT THE TOP OF THAT METHOD.
 
 
 // 5. What is the difference between evaluate expression and evaluate code fragment?
+//EVALUATE EXPRESSION IS A TOOL TO EVALUATE AND RUN A LINE OF CODE & CALL METHODS AND GIVE YOU THE RESULTS.
+// EVALUATE CODE FRAGMENT IS USED TO RUN MULTIPLE METHODS & MULTIPLE LINES OF CODE AND CREATE VARIABLES.
